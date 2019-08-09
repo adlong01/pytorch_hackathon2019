6b969c4f05160e5551041409a99827910b387c38
Suofei Feng <suofeif@stanford.edu>
2019-08-09 10:18:53 -0700

    text embedding

---
+++ Pytorch_Hack_copy/textmatching.py
@@ -0,0 +1,28 @@
+from six.moves import cPickle
+from text_feature import text_feature
+
+#!!!for the following: will download huge fasttext model to cache if it was not in the cache
+#comment it out if don't want to load it
+'''
+embedder = text_feature(mode = "single")
+'''
+
+'''
+#get the word embeddings for classes and save
+class_emb = embedder.preLoadVec("./coco_classes.txt")
+with open("class_vector.pkl", "wb") as f:
+    cPickle.dump(class_emb, f)
+print("saved class emb")
+'''
+#the following is to load vectors only for words in coco classes
+dict = None
+with open("class_vector.pkl", "rb") as f:
+    dict = cPickle.load(f)
+
+#print(dict['hair drier']) #some two-word phrases have word vectors
+
+'''
+#get word embeddings for queries
+query_emb = embedder.embQuery("cat")
+print(query_emb)
+'''
